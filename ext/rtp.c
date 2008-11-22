966174d56ad53b553be647eb9d6df175ddaf02fc
Jeremy Stephens <viking415@gmail.com>
2008-11-21 19:16:13 -0600

    Added remote addr/port to Session.new options
    - also added readers for remote_addr and remote_port

--- ext/rtp.c
+++ ext/rtp.c
@@ -5,7 +5,8 @@
 
 VALUE rb_mRTP;
 VALUE rb_cRSession;
-VALUE sym_mode;
+VALUE sym_mode, sym_remote;
+VALUE ip_regexp;
 
 static void
 session_free(ptr)
@@ -26,16 +27,18 @@ session_init(argc, argv, self)
   int argc;
   VALUE *argv, self;
 {
-  VALUE options, opt_mode;
+  VALUE options, opt_mode, opt_remote;
+  VALUE remote_addr;
   RtpSession *session;
-  int mode;
+  int mode, len, len2, remote_port;
+  char *str, *sptr;
 
   rb_scan_args(argc, argv, "01", &options);
   if (NIL_P(options)) {
     options = rb_hash_new();
   }
 
-  // session mode
+  // mode
   opt_mode = rb_hash_aref(options, sym_mode);
   if (NIL_P(opt_mode)) {
     mode = RTP_SESSION_SENDONLY;
@@ -47,7 +50,34 @@ session_init(argc, argv, self)
     }
   }
 
+  // remote address
+  opt_remote = rb_hash_aref(options, sym_remote);
+  if (NIL_P(opt_remote)) {
+    rb_raise(rb_eRuntimeError, "remote is required");
+  }
+  if (TYPE(opt_remote) != T_STRING) {
+    rb_raise(rb_eTypeError, "remote must be a string");
+  }
+  str  = RSTRING_PTR(opt_remote);
+  len  = RSTRING_LEN(opt_remote);
+  sptr = index(str, ':');
+  if (sptr == NULL) {
+    rb_raise(rb_eRuntimeError, "remote must have a valid port");
+  }
+  len2 = (int)(sptr - str);
+  remote_addr = rb_str_new(str, len2);
+  remote_port = atoi(sptr+1);
+  if (remote_port <= 0 || remote_port > 65535) {
+    rb_raise(rb_eRuntimeError, "remote must have a valid port");
+  }
+  if (!RTEST(rb_reg_match(ip_regexp, remote_addr))) {
+    rb_raise(rb_eRuntimeError, "remote must have a valid IP");
+  }
+  rb_iv_set(self, "@remote_addr", remote_addr);
+  rb_iv_set(self, "@remote_port", INT2FIX(remote_port));
+
   session = rtp_session_new(mode);
+  rtp_session_set_remote_addr(session, RSTRING_PTR(remote_addr), remote_port);
   DATA_PTR(self) = session;
   return self;
 }
@@ -65,6 +95,7 @@ Init_rtp()
   ortp_init();
 
   sym_mode = ID2SYM(rb_intern("mode"));
+  sym_remote = ID2SYM(rb_intern("remote"));
 
   rb_mRTP = rb_define_module("RTP");
   rb_cRSession = rb_define_class_under(rb_mRTP, "Session", rb_cObject);
@@ -72,9 +103,13 @@ Init_rtp()
   rb_define_const(rb_cRSession, "RTP_SESSION_RECVONLY", INT2FIX(RTP_SESSION_RECVONLY));
   rb_define_const(rb_cRSession, "RTP_SESSION_SENDONLY", INT2FIX(RTP_SESSION_SENDONLY));
   rb_define_const(rb_cRSession, "RTP_SESSION_SENDRECV", INT2FIX(RTP_SESSION_SENDRECV));
+  ip_regexp = rb_reg_new("^(?:\\d{1,3}\\.){3}\\d{1,3}$", 25, 0);
+  rb_define_const(rb_cRSession, "IP_REGEXP", ip_regexp);
 
   rb_define_alloc_func(rb_cRSession, session_alloc);
   rb_define_method(rb_cRSession, "initialize", session_init, -1);
+  rb_define_attr(rb_cRSession, "remote_addr", 1, 0);
+  rb_define_attr(rb_cRSession, "remote_port", 1, 0);
 
   rb_set_end_proc(rtp_shutdown, 0);
 }
--- test/test_rtp.rb
+++ test/test_rtp.rb
@@ -4,6 +4,8 @@ class TestRfp < MiniTest::Unit::TestCase
   class TestSession < MiniTest::Unit::TestCase
     def setup
       @valid_options = {
+        mode: RTP::Session::RTP_SESSION_SENDONLY,
+        remote: "127.0.0.1:31337"
       }
     end
 
@@ -26,6 +28,24 @@ class TestRfp < MiniTest::Unit::TestCase
       assert_raises(TypeError) { create_session(mode: -1) }
       assert_raises(TypeError) { create_session(mode: "pants") }
       create_session(mode: RTP::Session::RTP_SESSION_SENDRECV)
+      create_session(mode: nil)
+    end
+
+    def test_remote_addr_option
+      assert_raises(RuntimeError, "remote is required") { create_session(remote: nil) }
+      assert_raises(TypeError, "remote must be a string") { create_session(remote: 123.456) }
+      assert_raises(RuntimeError, "remote must have a valid port") { create_session(remote: "127.0.0.1") }
+      assert_raises(RuntimeError, "remote must have a valid port") { create_session(remote: "127.0.0.1:") }
+      assert_raises(RuntimeError, "remote must have a valid port") { create_session(remote: "127.0.0.1:-123") }
+      assert_raises(RuntimeError, "remote must have a valid port") { create_session(remote: "127.0.0.1:65536") }
+      assert_raises(RuntimeError, "remote must have a valid IP") { create_session(remote: "127.0.1:31337") }
+      assert_raises(RuntimeError, "remote must have a valid IP") { create_session(remote: ":31337") }
+      create_session(remote: "127.0.0.1:31337")
+    end
+
+    def test_remote_accessors
+      s = create_session
+      assert_equal("127.0.0.1", s.remote_addr)
     end
   end
 end
