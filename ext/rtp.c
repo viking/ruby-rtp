920cfc8751db56686a624d3bf9b2409e7e6fd264
Jeremy Stephens <viking415@gmail.com>
2008-11-21 22:55:14 -0600

    Added local address/port

--- ext/rtp.c
+++ ext/rtp.c
@@ -5,7 +5,7 @@
 
 VALUE rb_mRTP;
 VALUE rb_cRSession;
-VALUE sym_mode, sym_remote;
+VALUE sym_mode, sym_remote, sym_local;
 VALUE ip_regexp;
 
 static void
@@ -27,10 +27,10 @@ session_init(argc, argv, self)
   int argc;
   VALUE *argv, self;
 {
-  VALUE options, opt_mode, opt_remote;
-  VALUE remote_addr;
+  VALUE options, opt_mode, opt_remote, opt_local;
+  VALUE remote_addr, local_addr;
   RtpSession *session;
-  int mode, len, len2, remote_port;
+  int mode, len, len2, remote_port, local_port;
   char *str, *sptr;
 
   rb_scan_args(argc, argv, "01", &options);
@@ -76,7 +76,46 @@ session_init(argc, argv, self)
   rb_iv_set(self, "@remote_addr", remote_addr);
   rb_iv_set(self, "@remote_port", INT2FIX(remote_port));
 
+  // local address; isn't required for SENDONLY
+  opt_local = rb_hash_aref(options, sym_local);
+  if (!NIL_P(opt_local)) {
+    if (TYPE(opt_local) != T_STRING) {
+      rb_raise(rb_eTypeError, "local must be a string");
+    }
+    str  = RSTRING_PTR(opt_local);
+    len  = RSTRING_LEN(opt_local);
+    sptr = index(str, ':');
+    if (sptr == NULL) {
+      // port is not required for local connection
+      local_port = -1;
+      local_addr = rb_str_new(str, len);
+      rb_iv_set(self, "@local_port", Qnil);
+    }
+    else {
+      len2 = (int)(sptr - str);
+      local_addr = rb_str_new(str, len2);
+      local_port = atoi(sptr+1);
+      if (local_port <= 0 || local_port > 65535) {
+        rb_raise(rb_eRuntimeError, "local must have a valid port");
+      }
+      rb_iv_set(self, "@local_port", INT2FIX(local_port));
+    }
+    if (!RTEST(rb_reg_match(ip_regexp, local_addr))) {
+      rb_raise(rb_eRuntimeError, "local must have a valid IP");
+    }
+    rb_iv_set(self, "@local_addr", local_addr);
+  }
+  else {
+    if (NIL_P(opt_local) && mode != RTP_SESSION_SENDONLY) {
+      rb_raise(rb_eRuntimeError, "local is required");
+    }
+    local_addr = Qnil;
+  }
+
   session = rtp_session_new(mode);
+  if (RTEST(local_addr)) {
+    rtp_session_set_local_addr(session, RSTRING_PTR(local_addr), local_port);
+  }
   rtp_session_set_remote_addr(session, RSTRING_PTR(remote_addr), remote_port);
   DATA_PTR(self) = session;
   return self;
@@ -96,6 +135,7 @@ Init_rtp()
 
   sym_mode = ID2SYM(rb_intern("mode"));
   sym_remote = ID2SYM(rb_intern("remote"));
+  sym_local = ID2SYM(rb_intern("local"));
 
   rb_mRTP = rb_define_module("RTP");
   rb_cRSession = rb_define_class_under(rb_mRTP, "Session", rb_cObject);
@@ -110,6 +150,8 @@ Init_rtp()
   rb_define_method(rb_cRSession, "initialize", session_init, -1);
   rb_define_attr(rb_cRSession, "remote_addr", 1, 0);
   rb_define_attr(rb_cRSession, "remote_port", 1, 0);
+  rb_define_attr(rb_cRSession, "local_addr", 1, 0);
+  rb_define_attr(rb_cRSession, "local_port", 1, 0);
 
   rb_set_end_proc(rtp_shutdown, 0);
 }
--- test/test_rtp.rb
+++ test/test_rtp.rb
@@ -5,7 +5,8 @@ class TestRfp < MiniTest::Unit::TestCase
     def setup
       @valid_options = {
         mode: RTP::Session::RTP_SESSION_SENDONLY,
-        remote: "127.0.0.1:31337"
+        remote: "127.0.0.1:31337",
+        local:  "127.0.0.1:8000"
       }
     end
 
@@ -43,9 +44,33 @@ class TestRfp < MiniTest::Unit::TestCase
       create_session(remote: "127.0.0.1:31337")
     end
 
+    def test_local_addr_option
+      create_session(local: nil)
+      create_session(local: "127.0.0.1")
+      assert_raises(RuntimeError, "local is required") { create_session(local: nil, mode: 0) }
+      assert_raises(RuntimeError, "local is required") { create_session(local: nil, mode: 2) }
+      assert_raises(TypeError, "local must be a string") { create_session(local: 123.456) }
+      assert_raises(RuntimeError, "local must have a valid port") { create_session(local: "127.0.0.1:") }
+      assert_raises(RuntimeError, "local must have a valid port") { create_session(local: "127.0.0.1:-123") }
+      assert_raises(RuntimeError, "local must have a valid port") { create_session(local: "127.0.0.1:65536") }
+      assert_raises(RuntimeError, "local must have a valid IP") { create_session(local: "127.0.1:31337") }
+      assert_raises(RuntimeError, "local must have a valid IP") { create_session(local: ":31337") }
+    end
+
     def test_remote_accessors
       s = create_session
       assert_equal("127.0.0.1", s.remote_addr)
+      assert_equal(31337, s.remote_port)
+      assert_equal("127.0.0.1", s.local_addr)
+      assert_equal(8000, s.local_port)
+
+      s = create_session(local: nil)
+      assert_equal(nil, s.local_addr)
+      assert_equal(nil, s.local_port)
+
+      s = create_session(local: "127.0.0.1")
+      assert_equal("127.0.0.1", s.local_addr)
+      assert_equal(nil, s.local_port)
     end
   end
 end
